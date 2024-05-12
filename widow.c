a2b87d08658171ef1351079d290c4a6783ebd4d9
SAAD AMIR <saadamir318@gmail.com>
2024-05-13 00:57:09 +0500

    add messageBoxW
    anzi is outdate so using W wants us to specify the unicode wncoding by puting L before lp text/caption

--- widow.c
+++ widow.c
@@ -1,5 +1,11 @@
-#include <window.h>
+#include <windows.h>
  
 int main(void) {
-    return EXIT_SUCCESS;
+
+return MessageBoxW(
+        NULL,
+        L"Baby Steps for Mal Engg",    
+        L"Mal Engg. 101",
+        0x00000003L | MB_ICONASTERISK 
+    );
 }   
