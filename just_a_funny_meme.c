b4d190ca04b7873216f83407e51f6d0e8ad69260
SAAD AMIR <saadamir318@gmail.com>
2024-05-14 04:46:40 +0500

    add base setup for shellcode inject

--- just_a_funny_meme.c
+++ just_a_funny_meme.c
@@ -7,11 +7,20 @@
 #define warn(msg, ...) printf("[-] " msg "\n", ##__VA_ARGS__)
 
 
+DWORD PID = NULL;
+
 int main(int argc, char* argv[]){
     
-    printf("\n");
-    okay("okay macro woking fine");
-    info("info macro woking fine");
-    warn("warn macro woking fine");
+    if(argc < 2){
+        //argument count less than 2
+        warn("Usage: %s <PID>", argv[0]);
+        // argv[0] is always the name of th eprogram itself
+
+         return EXIT_FAILURE;
+    }
+
+    PID = atoi(argv[1]);
+    info("%s trying to open a handle to process: (%ld\n)", argv[0], PID);
+
     return EXIT_SUCCESS;
 }
